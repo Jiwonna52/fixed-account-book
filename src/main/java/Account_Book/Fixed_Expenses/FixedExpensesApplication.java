da7efffcf05aa6e0ab788c493d0ca1d49c9a9540
Jiwonna52 <Jiwonna52@gmail.com>
2024-01-02 12:38:54 +0900

    first commit

---
+++ src/main/java/Account_Book/Fixed_Expenses/FixedExpensesApplication.java
@@ -0,0 +1,13 @@
+package Account_Book.Fixed_Expenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixedExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixedExpensesApplication.class, args);
+	}
+
+}
